2983baf435762baf80b758550bf4bfdc5adbddc7
Dhruv Khanna <111333521+dhruvk0011@users.noreply.github.com>
2025-08-25 19:45:40 +0530

    Webcam Check

---
+++ main.py
@@ -0,0 +1,12 @@
+# Basic experimentation with OpenCV
+import cv2
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Test',frame)
+    if cv2.waitKey(1) & 0xFF == 27: # Press 'ESC' to exit
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+## Checkking for Webcam Access
